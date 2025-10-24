886adf5a1e7f0a4091400ee79fe5799798fe708d
daypher <daypher1@gmail.com>
2025-10-24 16:59:30 -0400

    verc

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
 	server: {
 		port: 1234,
 	},
-	base: process.env.NODE_ENV === "development" ? "/" : "/React-ProductList/",
 });
+
+// base: process.env.NODE_ENV === "development" ? "/" : "/React-ProductList/",
